5a204080363a8f454a404fc89e3a562589ef8214
PavanRangudu <pavanharishankar@gmail.com>
2017-03-14 20:58:18 +0530

    default_variable_initializations

---
+++ src/CJS3A3_example.java
@@ -0,0 +1,13 @@
+
+public class CJS3A3_example  {
+	int x; int y;
+	String name;
+	public static void main(String args[]) {
+	CJS3A3_example pnt = new CJS3A3_example();
+	System.out.println ("pnt is " + pnt.name + " " + pnt.x + " " + pnt.y);
+	} 
+}
+
+// Ans : d. The program prints pnt is null 0 0.
+
+
